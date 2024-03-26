6a4ef01317cca058d44cddb65be851812e55eb36
Rodolfo Milano <rodolfomilano@gmail.com>
2024-03-26 19:00:39 -0300

    General Update

--- my-movie-api/main.py
+++ my-movie-api/main.py
@@ -1,9 +1,42 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from config.database import engine, Base
+from middlewares.error_handler import ErrorHandler
+from routers.movie import movie_router
+from routers.user import user_router
 
 app = FastAPI()
 app.title = "Mi aplicación con FastAPI"
 app.version = "0.0.1"
 
+app.add_middleware(ErrorHandler)
+app.include_router(movie_router)
+app.include_router(user_router)
+
+Base.metadata.create_all(bind=engine)
+
+
+movies = [
+    {
+        "id": 1, 
+        "title": "Avatar", 
+        "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, ", 
+        "year": "2009", 
+        "rating": 7.8, 
+        "category": "Action"
+    },
+    {
+        "id": 2, 
+        "title": "Avatar", 
+        "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, ", 
+        "year": "2008", 
+        "rating": 7.8, 
+        "category": "Suspense"
+    }
+
+]
+
 @app.get('/', tags=['home'])
 def message():
-    return "Hello world!"
+    return HTMLResponse('<h1>Hello world!</h1>')
+
